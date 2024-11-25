f775d746eab56aade67e72f27b5fe49f94de7b1a
Javier Salcedo <146245197+JavierCedo@users.noreply.github.com>
2024-11-24 22:55:58 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -7,20 +7,21 @@ import seaborn as sns
 df_json = pd.read_json('prueba\\harmonized.json', lines=True)
 
 df1 = df_json[['spl_product_ndc', 'manufacturer_name', 'application_number', 'brand_name_suffix', 'spl_version','route']]
-df1.columns = ['spl_product_ndc', 'manufacturer_name', 'application_number', 'brand_name_suffix', 'spl_version','route']
 df2 = df_json[['generic_name']]
-df2.columns = ['generic_name']
 df3 = df_json[[ 'brand_name', 'upc','spl_set_id', 'product_ndc','original_packager_product_ndc']]
-df3.columns = [ 'brand_name', 'upc','spl_set_id', 'product_ndc','original_packager_product_ndc']
 df4 = df_json[['substance_name']]
-df4.columns = ['substance_name']
 df5 = df_json[['unii_indexing']]
-df5.columns = ['unii_indexing']
 df6 = df_json[['package_ndc','product_type']]
-df6.columns = ['package_ndc','product_type']
 df7 = df_json[['rxnorm']]
-df7.columns = ['rxnorm']
 df8 = df_json[['is_original_packager','id','dosage_form']]
+
+df1.columns = ['spl_product_ndc', 'manufacturer_name', 'application_number', 'brand_name_suffix', 'spl_version','route']
+df2.columns = ['generic_name']
+df3.columns = [ 'brand_name', 'upc','spl_set_id', 'product_ndc','original_packager_product_ndc']
+df4.columns = ['substance_name']
+df5.columns = ['unii_indexing']
+df6.columns = ['package_ndc','product_type']
+df7.columns = ['rxnorm']
 df8.columns = ['is_original_packager','id','dosage_form']
 
 print('df1');print(tabulate(df1.head(5),headers='keys'))
@@ -60,9 +61,6 @@ print(grafica_top_10(separar_columna_en_columnas('route'), 'route'))
 print(grafica_top_10(separar_columna_en_columnas('generic_name'),'generic_name'))
 print(grafica_top_10(separar_columna_en_columnas('brand_name'),'brand_name'))
 print(grafica_top_10(separar_columna_en_columnas('substance_name'),'substance_name'))
-print('Listo')
-
-
 
 
 
@@ -75,8 +73,6 @@ df2; generic_name, separarlos y ver cuales son los mas comunes.
 df3; brand_name, ver si se repiten
 df4; substance_name, principal activo ,separar las sustancias y ver cuales son las mas usadas
 '''
-
-
 
 ''' ## Ideas pendientes
 
