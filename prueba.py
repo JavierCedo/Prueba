b7d5e2bc30ed7ed897b9ae40396b251a8c90ca07
Javier Salcedo <146245197+JavierCedo@users.noreply.github.com>
2024-11-26 23:32:10 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from tabulate import tabulate
@@ -12,9 +13,9 @@ import re
 os.makedirs("carpeta_de_extraccion", exist_ok=True) 
 patoolib.extract_archive("prueba.rar", outdir="carpeta_de_extraccion")
 
+
 # Leer el archivo JSON en un DataFrame de pandas
 df_json = pd.read_json('carpeta_de_extraccion/prueba/harmonized.json', lines=True) 
-
 
 #################### Graficas y analicis de datos ####################
 
@@ -24,30 +25,21 @@ def transformar_texto(serie):
     df_json[serie] = df_json[serie].str.upper().str.replace(r'[.,;:]', '', regex=True).str.replace(r' INC ', '', regex=True).str.replace(r' INC', '', regex=True)
     return df_json[serie]
 
-# Asi se tiene que buscar la funcion    transformar_texto
-transformar_texto('manufacturer_name')
+# Asi se tiene que buscar la funcion
 transformar_texto('generic_name')               #Arreglar
 transformar_texto('manufacturer_name')
-transformar_texto('brand_name')                 #Arreglar
-transformar_texto('substance_name')             #Arreglar
 transformar_texto('dosage_form')
 
-
-
-def separar_columna_en_columnas(serie, delimitadores=None):
+def conteo_de_elementos(serie, delimitadores=None):
     if delimitadores:
         delimitador_regex = '|'.join(map(re.escape, delimitadores))
-        nuevas_columnas = df_json[serie].str.split(delimitador_regex, expand=True)
+        nuevas_columnas = serie.str.split(delimitador_regex, expand=True)
         value_counts = nuevas_columnas.apply(pd.Series.value_counts).sum(axis=1).astype(int)
         value_counts = value_counts[value_counts.index.notna()]
         value_counts = value_counts.drop('', errors='ignore')
-        for i, j in value_counts.items():
-            print(i,j)
     else:
-        value_counts = df_json[serie].value_counts().astype(int)
-        for i, j in value_counts.items():
-            print(i,j)
-    return value_counts.head(50)
+        value_counts = serie.value_counts().astype(int)
+    return value_counts
 
 def grafica_top_10(serie, titulo): 
     top_10 = serie.nlargest(10) 
@@ -63,43 +55,115 @@ def grafica_top_10(serie, titulo):
     plt.tight_layout()
     plt.show()
 
-
-
 ## Pruebas de separacion y graficas
 print('manufacturer_name', '#'*60)
-print(grafica_top_10(separar_columna_en_columnas('manufacturer_name'),'Nombre de la grafica'))
+print(grafica_top_10(conteo_de_elementos(df_json['manufacturer_name']),'Nombre de la grafica'))
 
 print('route', '#'*60)
 delimitadores = ['; ']
-print(grafica_top_10(separar_columna_en_columnas('route',delimitadores), 'route'))
+print(grafica_top_10(conteo_de_elementos(df_json['route'],delimitadores), 'route'))
 
 print('generic_name', '#'*60)
 delimitadores = [', ', ' - ', ' and ', 'and ', ',', ' AND ','AND ' , '-']
-print(grafica_top_10(separar_columna_en_columnas('generic_name',delimitadores),'generic_name'))
+print(grafica_top_10(conteo_de_elementos(df_json['generic_name'],delimitadores),'generic_name'))
 
 print('brand_name', '#'*60)
 delimitadores = [' and ', ' AND ', ' - ']
-print(grafica_top_10(separar_columna_en_columnas('brand_name',delimitadores),'brand_name'))
+print(grafica_top_10(conteo_de_elementos(df_json['brand_name'],delimitadores),'brand_name'))
 
 print('substance_name', '#'*60)
 delimitadores = ['; ']
-print(grafica_top_10(separar_columna_en_columnas('substance_name',delimitadores),'substance_name'))
+print(grafica_top_10(conteo_de_elementos(df_json['substance_name'],delimitadores),'substance_name'))
 
 print('dosage_form', '#'*60)
 delimitadores = [', ','/','/ ',' / ']
-print(grafica_top_10(separar_columna_en_columnas('dosage_form',delimitadores),'dosage_form'))
+print(grafica_top_10(conteo_de_elementos(df_json['dosage_form'],delimitadores),'dosage_form'))
 
 
+#################### Busqueda de medicamento ####################
+
+df_bus = df_json[['brand_name', 'substance_name']]
+df_bus.columns = ['brand_name', 'substance_name']
+df_bus = df_bus.drop_duplicates()
+
+def separar_componentes(serie, delimitadores=None):
+    if delimitadores:
+        delimitador_regex = ' '.join(map(re.escape, delimitadores))
+        df_bus_new = serie.str.split(delimitador_regex, expand=True)
+    else:
+        df_bus_new = serie.str.split(expand=True)
+
+    # Renombrar las nuevas columnas para mejor visualización
+    df_bus_new.columns = [f'Componente_{i+1}' for i in range(df_bus_new.shape[1])]
+    
+    return df_bus_new
+
+# Aplicar la función a la columna substance_name
+df_bus_new = separar_componentes(df_bus['substance_name'], ['; '])
+df_bus_new['brand_name'] = df_bus['brand_name']
+df_bus_new = df_bus_new.set_index('brand_name')
+
+### Farmacias Ficticias
+
+df1 = df_bus_new.sample(n=len(df_bus_new)//3, replace=True)
+df2 = df_bus_new.sample(n=len(df_bus_new)//3, replace=True)
+df3 = df_bus_new.sample(n=len(df_bus_new)//3, replace=True)
+
+list_farms = {'Farmacia1':df1, 'Farmacia2':df2, 'Farmacia3':df3}
+
+# Medicamento a buscar
+med = "Clean Force"
+
+# Almacenar datos de los resultados de búsqueda
+farm_esta_med = {}
+farm_y_comp = {}
+farm_no_med = {}
+far_med_sml = {}
+
+# Verificar la presencia del medicamento, obtener los componentes y saber en que farmacia no esta
+for nom_farm, df_farm in list_farms.items():
+    if med in df_farm.index:
+        #print('Esta en', nom_farm)
+        farm_esta_med[nom_farm] = f'Esta el medicamento {med}'
+        df_bus_new = df_bus_new.loc[med]
+        value_counts = df_bus_new.apply(pd.Series.value_counts).sum(axis=1).astype(int).index.tolist()
+        #print(value_counts)
+        farm_y_comp[nom_farm] = value_counts
+    else:
+        farm_no_med[nom_farm] = f'No esta el medicamento {med}'
+
+# Verificar en la farmacia que no se encontro si existe uno similar
+if farm_esta_med:
+    indices_lista = []
+    def encontrar_primera_lista():
+        for nombre, df in list_farms.items():
+            for nom_far_no in farm_no_med:
+                for nom_far, lista in farm_y_comp.items():
+                    for i in lista:
+                        indices = df.apply(lambda row: row == i, axis=1).any(axis=1)
+                        indices_lista = df[indices].index.tolist()[:5]  # Guardar solo los primeros 5 resultados
+                        if indices_lista:
+                            far_med_sml[nom_far_no] = indices_lista
+                            yield indices_lista  # Usar yield para retornar la lista y salir del generador
+    indices_lista = next(encontrar_primera_lista(), [])
+elif not farm_esta_med:
+    print('No se encontro para comparar')
 
 
-''' ## Ideas pendientes
-
-# Leer el archivo de texto en un DataFrame de pandas
-df_txt = pd.read_csv('prueba\\Products.txt', sep='\t', on_bad_lines='skip')
-print('#'*20,"Archivo .txt",'#'*20)
-print(df_txt.info())
-df1 = df_txt[['ApplNo','ProductNo','Form','Strength','ReferenceDrug', 'DrugName']]
-df2 = df_txt[['ActiveIngredient','ReferenceStandard']]
-print('df1');print(tabulate(df1.head(5),headers='keys'))
-print('df2');print(tabulate(df2.head(5),headers='keys'))
-'''
+print("Medicamento a buscar:", med)
+print("En que farmacia esta:")
+df_farm_esta_med = pd.DataFrame(list(farm_esta_med.items()), columns=['Farmacia', 'Estado'])
+df_farm_esta_med = df_farm_esta_med.set_index('Farmacia')
+print(df_farm_esta_med)
+print("Componentes por farmacia:")
+df_farm_y_comp = pd.DataFrame(list(farm_y_comp.items()), columns=['Farmacia', 'Estado'])
+df_farm_y_comp = df_farm_y_comp.set_index('Farmacia')
+print(df_farm_y_comp)
+print("En que farmacia no esta:")
+df_farm_no_med = pd.DataFrame(list(farm_no_med.items()), columns=['Farmacia', 'Estado'])
+df_farm_no_med = df_farm_no_med.set_index('Farmacia')
+print(df_farm_no_med)
+print("Farmacia donde esta un medicamento similar:")
+df_far_med_sml = pd.DataFrame(list(far_med_sml.items()), columns=['Farmacia', 'Estado'])
+df_far_med_sml = df_far_med_sml.set_index('Farmacia')
+print(df_far_med_sml)
