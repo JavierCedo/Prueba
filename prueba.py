eb3ae01c6c77672a0a21830a0c9c4d716055c685
Javier Salcedo <146245197+JavierCedo@users.noreply.github.com>
2024-11-29 00:01:43 -0400

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from tabulate import tabulate
@@ -13,19 +12,24 @@ import re
 os.makedirs("carpeta_de_extraccion", exist_ok=True) 
 patoolib.extract_archive("prueba.rar", outdir="carpeta_de_extraccion")
 
-
 # Leer el archivo JSON en un DataFrame de pandas
 df_json = pd.read_json('carpeta_de_extraccion/prueba/harmonized.json', lines=True) 
 
+
 #################### Graficas y analicis de datos ####################
 
-# Funciones
 
 def transformar_texto(serie): 
     df_json[serie] = df_json[serie].str.upper().str.replace(r'[.,;:]', '', regex=True).str.replace(r' INC ', '', regex=True).str.replace(r' INC', '', regex=True)
     return df_json[serie]
 
-# Asi se tiene que buscar la funcion
+def eliminar_numeros(serie): 
+    df_json[serie] = df_json[serie].str.upper().str.replace(r'[0123456789]', '', regex=True)
+    df_json[serie] = df_json[serie].replace(r'^\s*$', pd.NA, regex=True)
+    df_json[serie] = df_json[serie].dropna()
+    return df_json[serie]
+
+eliminar_numeros('application_number')
 transformar_texto('generic_name')               #Arreglar
 transformar_texto('manufacturer_name')
 transformar_texto('dosage_form')
@@ -41,8 +45,8 @@ def conteo_de_elementos(serie, delimitadores=None):
         value_counts = serie.value_counts().astype(int)
     return value_counts
 
-def grafica_top_10(serie, titulo): 
-    top_10 = serie.nlargest(10) 
+def grafica_top_10_barras(serie, titulo): 
+    top_10 = serie.dropna().nlargest(10)
     sns.set(style="whitegrid")
     plt.figure(figsize=(14, 6))
     ax = sns.barplot(x=top_10.values, y=top_10.index, palette="viridis") 
@@ -55,32 +59,83 @@ def grafica_top_10(serie, titulo):
     plt.tight_layout()
     plt.show()
 
-## Pruebas de separacion y graficas
-print('manufacturer_name', '#'*60)
-print(grafica_top_10(conteo_de_elementos(df_json['manufacturer_name']),'Nombre de la grafica'))
+def grafica_comparativa(serie, titulo):
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(14, 6))
+    ax = sns.barplot(x=serie.values, y=serie.index, palette="viridis")
+    ax.set_title(f'Comparación de Series Combinadas - {titulo}', fontsize=15, pad=20)
+    ax.set_xlabel('Valores', fontsize=12)
+    ax.set_ylabel('Índices Combinados', fontsize=12)
+    for i, (value, name) in enumerate(zip(serie.values, serie.index)):
+        ax.text(value, i, f' {int(value)}', color='black', va='center')
+    plt.tight_layout()
+    plt.show()
 
-print('route', '#'*60)
-delimitadores = ['; ']
-print(grafica_top_10(conteo_de_elementos(df_json['route'],delimitadores), 'route'))
+def grafica_top_10_histograma(serie, titulo): 
+    top_10 = serie.nlargest(10)
+    sns.set(style="whitegrid") 
+    plt.figure(figsize=(14, 8)) 
+    colors = sns.color_palette("viridis", len(top_10)) 
+    ax = plt.gca() 
+    for i, (value, name) in enumerate(zip(top_10.values, top_10.index)): 
+        ax.bar(name, value, color=colors[i]) 
+        ax.text(i, value, f'{value}', ha='center', color='black')
+    ax.set_title(f'Top 10 {titulo}', fontsize=15, pad=20) 
+    ax.set_xlabel('Elementos', fontsize=12) 
+    ax.set_ylabel('Frecuencia', fontsize=12) 
+    plt.xticks(rotation=45) 
+    plt.tight_layout() 
+    plt.show()
 
-print('generic_name', '#'*60)
-delimitadores = [', ', ' - ', ' and ', 'and ', ',', ' AND ','AND ' , '-']
-print(grafica_top_10(conteo_de_elementos(df_json['generic_name'],delimitadores),'generic_name'))
+def grafica_top_10_pastel(serie, titulo): 
+    serie = serie.dropna()
+    top_10 = serie.nlargest(10)
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 10))
+    if len(top_10) == 2:
+        colors = ['#482173', '#c2df23']
+    else:
+        colors = sns.color_palette("viridis", len(top_10))
+    plt.pie(top_10.values, labels=top_10.index, autopct='%1.1f%%', startangle=140, colors=colors)
+    plt.title(f'Top 10 {titulo}', fontsize=15, pad=20)
+    plt.axis('equal')
+    plt.show()
 
-print('brand_name', '#'*60)
-delimitadores = [' and ', ' AND ', ' - ']
-print(grafica_top_10(conteo_de_elementos(df_json['brand_name'],delimitadores),'brand_name'))
+def combinar_series(serie1, serie2):
+    serie1 = serie1.value_counts().nlargest(10)
+    serie2 = serie2.value_counts().nlargest(10)
+    serie_combinada = pd.concat([serie1, serie2], axis=1, keys=['Serie 1', 'Serie 2']).fillna(0)
+    serie_combinada['Combinada'] = serie_combinada['Serie 1'] + serie_combinada['Serie 2']
+    serie_final = serie_combinada['Combinada']
+    return serie_final
 
-print('substance_name', '#'*60)
-delimitadores = ['; ']
-print(grafica_top_10(conteo_de_elementos(df_json['substance_name'],delimitadores),'substance_name'))
 
-print('dosage_form', '#'*60)
+grafica_top_10_histograma(conteo_de_elementos(df_json['application_number']),'application_number')
+
+serie_combinada = combinar_series(df_json['dosage_form'], df_json['product_type'])
+grafica_comparativa(serie_combinada, 'Gráfica Comparativa de Series Combinadas')
+
 delimitadores = [', ','/','/ ',' / ']
-print(grafica_top_10(conteo_de_elementos(df_json['dosage_form'],delimitadores),'dosage_form'))
+grafica_top_10_histograma(conteo_de_elementos(df_json['dosage_form'],delimitadores),'dosage_form')
+
+grafica_top_10_barras(conteo_de_elementos(df_json['manufacturer_name']),'Grafica de barras')
+
+grafica_top_10_pastel(conteo_de_elementos(df_json["product_type"]), 'Grafica de histograma')
+
+grafica_top_10_pastel(conteo_de_elementos(df_json['is_original_packager']),'is_original_packager')
+
+delimitadores = ['; ']
+grafica_top_10_barras(conteo_de_elementos(df_json['substance_name'], delimitadores),'Grafica de barras')
+
+delimitadores = ['; ']
+grafica_top_10_histograma(conteo_de_elementos(df_json['route'],delimitadores), 'route')
+
+delimitadores = [', ', ' - ', ' and ', 'and ', ',', ' AND ','AND ' , '-']
+grafica_top_10_pastel(conteo_de_elementos(df_json['generic_name'],delimitadores),'generic_name')
 
 
 #################### Busqueda de medicamento ####################
+
 
 df_bus = df_json[['brand_name', 'substance_name']]
 df_bus.columns = ['brand_name', 'substance_name']
@@ -92,10 +147,7 @@ def separar_componentes(serie, delimitadores=None):
         df_bus_new = serie.str.split(delimitador_regex, expand=True)
     else:
         df_bus_new = serie.str.split(expand=True)
-
-    # Renombrar las nuevas columnas para mejor visualización
     df_bus_new.columns = [f'Componente_{i+1}' for i in range(df_bus_new.shape[1])]
-    
     return df_bus_new
 
 # Aplicar la función a la columna substance_name
@@ -123,14 +175,16 @@ far_med_sml = {}
 # Verificar la presencia del medicamento, obtener los componentes y saber en que farmacia no esta
 for nom_farm, df_farm in list_farms.items():
     if med in df_farm.index:
-        #print('Esta en', nom_farm)
         farm_esta_med[nom_farm] = f'Esta el medicamento {med}'
         df_bus_new = df_bus_new.loc[med]
         value_counts = df_bus_new.apply(pd.Series.value_counts).sum(axis=1).astype(int).index.tolist()
-        #print(value_counts)
         farm_y_comp[nom_farm] = value_counts
     else:
         farm_no_med[nom_farm] = f'No esta el medicamento {med}'
+
+if not farm_esta_med:
+    farm_esta_med[nom_farm] = f'No asta el medicamento {med}'
+    farm_y_comp[nom_farm] = f'Esta el medicamento {med}'
 
 # Verificar en la farmacia que no se encontro si existe uno similar
 if farm_esta_med:
@@ -149,21 +203,19 @@ if farm_esta_med:
 elif not farm_esta_med:
     print('No se encontro para comparar')
 
+if not farm_no_med:
+    farm_no_med[nom_farm] = f'Se encontro en todas las farmacias {med}'
+    far_med_sml[nom_farm] = f'Se encontro en todas las farmacias {med}'
 
+## Salida
+def print_df_with_title(title, df_dict):
+    df = pd.DataFrame(list(df_dict.items()), columns=['Farmacia', 'Estado'])
+    print("-"*60)
+    print(title)
+    print(df.to_string(index=False, header=False))
+    
 print("Medicamento a buscar:", med)
-print("En que farmacia esta:")
-df_farm_esta_med = pd.DataFrame(list(farm_esta_med.items()), columns=['Farmacia', 'Estado'])
-df_farm_esta_med = df_farm_esta_med.set_index('Farmacia')
-print(df_farm_esta_med)
-print("Componentes por farmacia:")
-df_farm_y_comp = pd.DataFrame(list(farm_y_comp.items()), columns=['Farmacia', 'Estado'])
-df_farm_y_comp = df_farm_y_comp.set_index('Farmacia')
-print(df_farm_y_comp)
-print("En que farmacia no esta:")
-df_farm_no_med = pd.DataFrame(list(farm_no_med.items()), columns=['Farmacia', 'Estado'])
-df_farm_no_med = df_farm_no_med.set_index('Farmacia')
-print(df_farm_no_med)
-print("Farmacia donde esta un medicamento similar:")
-df_far_med_sml = pd.DataFrame(list(far_med_sml.items()), columns=['Farmacia', 'Estado'])
-df_far_med_sml = df_far_med_sml.set_index('Farmacia')
-print(df_far_med_sml)
+print_df_with_title("En qua farmacia esta:", farm_esta_med)
+print_df_with_title("Componentes por farmacia:", farm_y_comp)
+print_df_with_title("En qua farmacia no esta:", farm_no_med)
+print_df_with_title("Farmacia donde esta un medicamento similar:", far_med_sml)
